af50b55d203e406e6d41a8cc62a614b73aac654c
Bram-R <67429090+Bram-R@users.noreply.github.com>
2021-02-09 09:58:08 +0100

    Remove redundant code

--- app.R
+++ app.R
@@ -1,24 +1,19 @@
 ###### Setup ######
 rm(list = ls())
-Sys.setlocale("LC_TIME", "Dutch") # set to Dutch locale (to get Dutch month names) for this session
-options(scipen = 999)
 
 # load packages
-library("data.table")
-library("rms")
-library("forecast")
-library("zoo")
-library("tidyverse")
 library("rjson")
 library("pdftools")
-library("magick")
+library("data.table")
+library("rms")
+library("zoo")
+library("tidyverse")
 library("shiny")
 library("shinydashboard")
 
 # load functions and shinyApp server + graphical user interface
 source("f_data.R") # function to obtain and process (mainly NL) Covid-19 data 
 source("f_pdf_rivm_test.R") # function to extract test data from RIVM report
-# source("f_trend.R") # function to estimate and extrapolate trends over time 
 source("f_figure.R") # function to make Figures based on (mainly NL) Covid-19 data 
 source("f_server.R")
 source("ui.R") # source shiny graphical user interface
